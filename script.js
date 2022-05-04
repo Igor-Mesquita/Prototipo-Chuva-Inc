f974d5c16d4043f2e3ab60f10c5f99d09ebb19b2
Igor Mesquita <igormesquita2701@gmail.com>
2022-05-04 04:37:39 -0300

    2

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function topics() {
-  let div = []
-  
+var btn = document.querySelector('#btn-topic')
+var div = document.querySelector('.card-script')
+var oldTopic = document.querySelector('.topics-text')
+btn.addEventListener('click', function(){
+  if (div.style.display === "none") { 
+    div.style.display = "block"
+    oldTopic.style.display = 'none'
+  }
+})
 
-}
